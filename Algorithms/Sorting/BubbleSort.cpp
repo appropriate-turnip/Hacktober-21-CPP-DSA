660e604ed4653482c3974e00a3c30c520cc74a23
Tushar630 <tusharagrawal901@gmail.com>
2022-10-20 10:22:08 +0530

    few lines changed

--- Algorithms/Sorting/BubbleSort.cpp
+++ Algorithms/Sorting/BubbleSort.cpp
@@ -10,11 +10,36 @@ int main()
     {
         for (int j = i + 1; j < l - 1; j++)
         {
-            int temp = arr[i];
-            if (arr[j] < temp)
+            if(arr[j]<arr[i])
             {
-                arr[i]=arr[j];
-                arr[j]=temp;
+                arr[j]+=arr[i];
+                arr[i]=arr[j]-arr[i];
+                arr[j]=arr[j]-arr[i];
+            }
+        }
+    }
+    for (int j = 0; j < l; j++)
+    {
+        cout<<arr[j]<<" ";
+    }
+    
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int arr[] = {2, 5, 3, 1, 6, 7, 9, 4, 8, 10};
+    int l = sizeof(arr) / sizeof(*arr);
+    for (int i = 0; i < l; i++)
+    {
+        for (int j = i + 1; j < l - 1; j++)
+        {
+            if(arr[j]<arr[i])
+            {
+                arr[j]+=arr[i];
+                arr[i]=arr[j]-arr[i];
+                arr[j]=arr[j]-arr[i];
             }
         }
     }
